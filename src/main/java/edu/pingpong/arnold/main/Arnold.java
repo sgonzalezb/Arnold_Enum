0b7e0a0129e6c70c1c54b1731c1b2ebdbff37f31
Sergio <sgonzalezb@cifpfbmoll.eu>
2021-03-04 18:56:37 +0100

    enum planeta

---
+++ src/main/java/edu/pingpong/arnold/main/Arnold.java
@@ -0,0 +1,27 @@
+package edu.pingpong.arnold.main;
+
+import edu.pingpong.arnold.logica.Planeta;
+
+public class Arnold {
+	
+	 public static void main(String[] args) {
+		 
+		 double peso = 1.0; // kg
+		 
+         for(Planeta planeta : Planeta.values()){
+        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+         }
+         
+         System.out.println("\nYour weight only on the terrestrial planets: ");
+         
+         for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+         }
+         
+         System.out.println("\nYour weight only on the gas giant planets: ");
+         for(Planeta planeta: Planeta.getGigantesGaseosos()){
+        	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+         }
+	 }
+
+}
